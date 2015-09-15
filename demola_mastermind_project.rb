be2ebafb7bc5e199e562c7caaf3754f1d095a317
Emmanuel Ade <emmade.tx@gmail.com>
2015-09-15 20:29:36 +0100

    Getting ahead2

--- demola_mastermind_project.rb
+++ demola_mastermind_project.rb
@@ -1,37 +1,40 @@
 class Computer
-   @generated_code = @generated_code
 
-  def code_beginner
-    colors = ['B', 'G', 'Y', 'R']
+  def self.code_beginner
+    colors = ['b', 'g', 'y', 'r']
     colors_rand = Random.new
-    @generated_code = []
-    4.times{ @generated_code << colors[rand(4)] }
-    @generated_code
+    generated_code = []
+    4.times{ generated_code << colors[rand(4)] }
+    generated_code
   end
 end
 
 class GameEngine
+
   def play_calc_exact_partial
         start_time=Time.now
+        generated_code = Computer.code_beginner
+
           10.times do |i|
+
           guess_one = (gets.chomp).split('')
-            @generated_code
+
             counter = 0
             counter_partial = 0
-            check = @generated_code.zip(guess_one)
+            check = generated_code.zip(guess_one)
               check.each do |i|
               if i[0] == i[1]
                   counter += 1
               else
-                  if @generated_code.include?i[1]
+                  if generated_code.include?i[1]
                       counter_partial += 1
                   end
               end
           end
 
-    if counter == @generated_code.length
+    if counter == generated_code.length
              end_time=Time.now
-             puts "Congratulations! You guessed the sequence #{@generated_code} in #{end_time-start_time}\n
+             puts "Congratulations! You guessed the sequence #{generated_code} in #{end_time-start_time}\n
 Do you want to (p)lay again or (q)uit?"
 break
 else
@@ -59,7 +62,4 @@ class Player_two
 end
 a = Computer.new
 b = GameEngine.new
-a.code_beginner
-b.code_beginner
-a.play_calc_exact_partial
 b.play_calc_exact_partial
--- welcome_message.rb
+++ welcome_message.rb
@@ -1,3 +1,4 @@
+require_relative 'demola_mastermind_project'
 class WelcomeMessage
   def introduction
       puts "Welcome to MASTERMIND!\n
@@ -15,10 +16,10 @@ Enter (3) for Advanced."
       puts "So you would like to play the game!,\n start by telling me your name\n"
         name = gets.chomp
         puts "#{name}, I have generated a beginner sequence with four elements made up of:
-(y)ellow".yellow", (c)yan".cyan", (o)range".orange" and (r)ed".red "You are to guess the sequence in which the
+(y)ellow.yellow, (c)yan.cyan, (o)range.orange and (r)ed.red You are to guess the sequence in which the
 se colors appeared e.g YCOR for (y)ellow, (c)yan, (o)range and (r)ed. You have 10 guesses to get these colors or you lose the game.
 Ready to play?"
-    Player_one.guess_code_one
+    GameEngine.new.play_calc_exact_partial
 elsif level == "2"
   puts "So you would like to play the game!,\n start by telling me your name\n"
     name = gets.chomp
@@ -26,7 +27,7 @@ elsif level == "2"
 (y)ellow, (c)yan, (o)range, (r)ed,(v)iolet . You are to guess the sequence in which the
 se colors appeared e.g YCOR for (y)ellow, (c)yan, (o)range and (r)ed, (v)iolet. You have 10 guesses to get these colors or you lose the game.
 Ready to play?"
-    Player_one.guess_code_one
+    GameEngine.new.play_calc_exact_partial
 elsif level == "3"
   puts "So you would like to play the game!,\n start by telling me your name\n"
     name = gets.chomp
@@ -34,14 +35,14 @@ elsif level == "3"
 (y)ellow, (c)yan, (o)range, (r)ed,(v)iolet . You are to guess the sequence in which the
 se colors appeared e.g YCOR for (y)ellow, (c)yan, (o)range and (r)ed, (v)iolet, (i)ndigo. You have 10 guesses to get these colors or you lose the game.
 Ready to play?"
-    Player_one.guess_code_one
+      GameEngine.new.play_calc_exact_partial
 else
   puts "You have entered an invalid input"
 end
    when "i" then puts "Enter a sequence of 4 colors containing the generated colors e.g RYBG or YGRB.
 If you enter fewer than 4 or more than 4 colors, you would receive an error mess
 age"
-
+WelcomeMessage.new.introduction
 when "b" then puts "Just a little background on MASTERMIND Mastermind is a board game with an
 interesting history (or rather a legend?). Some game books report that it was in
 vented in 1971 by Mordecai Meirowitz, an Israeli postmaster and telecommunicatio
@@ -53,9 +54,10 @@ milar game called 'Bulls and cows' in English, and 'Numerello' in Italian... Act
 ually, the old British game 'Bulls and cows' was somewhat different from the com
 mercial version. It was played on paper, not on a board... Over 50 million copie
 s later, Mastermind is still marketed today!"
-
+WelcomeMessage.new.introduction
   when "q" then puts "Thank you for playing Mastermind\n Good byebye"
     system('exit')
 end
   end
 end
+WelcomeMessage.new.introduction
