a2dc6bd89ced4872b2241518a8247c6a3fd14353
Emmanuel Ade <emmade.tx@gmail.com>
2015-09-19 14:26:51 +0100

    new repo

--- demola_mastermind_project.rb
+++ demola_mastermind_project.rb
@@ -1,9 +1,8 @@
- require_relative 'welcome_message'
+require_relative 'welcome_message'
 require 'colorize'
 require 'json'
 require 'pry'
 class Computer #the computer class has 3 methods which generate 3 different codes for each level
-
 #this method generates 4 random codes
   def self.code_beginner
     colors = ['y', 'c', 'b', 'r']
@@ -46,10 +45,14 @@ class GameEngine #this is the engine of the game as the name implies, it gets th
 
         start_time=Time.now # this line of code records the starting time
         generated_code = Difficulty.code_level level  #this line of code gets the computer generated code for any the level as picked by the user
-        p generated_code
+
           10.times do |i| # this loop is to record the number of times the user can guess at each time, the partial and exact calculation are ongoing within the loop
 
           guess_one = (gets.chomp).split('') #this line of code gets the user's guess and convert it into an array
+          if guess_one == "ch".split('')
+            p generated_code
+          end
+
             exact_match = 0 #counter for exact match
             partial_match = 0 #counter for partial match
             check = generated_code.zip(guess_one) #this line of code combine merges the computer generated code and the guess code and the output is saved into a check variable
@@ -73,7 +76,7 @@ class GameEngine #this is the engine of the game as the name implies, it gets th
              break # at the point where the exact match equals the computer code length, the loop breaks, if not the loops continues
     else
     puts "#{guess_one} ".cyan + " has " + "#{partial_match} ".magenta + " partial match with " + "#{exact_match} ".blue + "
-    exact match in the correct positions. You have taken " + "#{i+1}".cyan
+    exact match in the correct positions. You have taken " + "#{i+1}".cyan + " attempt or guess(s)"
 
         end
     end
@@ -86,44 +89,53 @@ class GameEngine #this is the engine of the game as the name implies, it gets th
        puts "Thank you for playing Mastermind\n Good byebye".blue
      system(exit) #this code breaks the whole of my code and leaves the entire game
    end
-
 end
 end
-class TopScore
-    def self.topscore_file name_convert, finished_time
+class TopScore #this class have only one method, contains IO file that reads the name and time of the player to be able to set top ten
+    def self.topscore_file name_convert, finished_time  #collect the name and finished time from the user
 
-        file = File.read("score.json")
-        score = JSON.parse(file)
+        file = File.read("score.json")  #this IO file read from json file score.json
+        score = JSON.parse(file)   #the line of code convert the json file into hash
         len = score["scores"].length
 
-        record = Hash.new
-        record["name"] = name_convert
-        record["time"] = finished_time
-        score["scores"][len] = record
+        record = Hash.new # create a new hash
+        record["name"] = name_convert # save the name of the user in a key
+        record["time"] = finished_time # save the finished time in the a key
+        score["scores"][len] = record # put the the new generated elements in the new hash
 
-        name_score = score.to_json
+        name_score = score.to_json # converted back to json
 
+            #saving the converted json file into its proper place by writing in it
         file = File.new("score.json", 'w')
         file.write(name_score)
         file.close
 
-        file = File.read("score.json")
-        top_score = JSON.parse(file)
+        file = File.read("score.json") # reading the new file
+        top_score = JSON.parse(file) #converting into hash for usage
 
-        top_scores = top_score["scores"].sort_by {|x| x["time"]}
+        top_scores = top_score["scores"].sort_by {|x| x["time"]} #iterating through the hash and sorting it with time
           puts "Do you want to view the top score? type" + " y".blue + " for yes," + " N".magenta + " for no and" + " q".red + " to quit the game"
           reply = gets.chomp
           if reply == "y"
           #binding.pry
+          puts "========================="
           puts "******TOP SCORES******"
-          top_scores.each do |x|
+          puts "========================="
+          top_scores.each do |x| #iterating through the hash to get the the element in order to print the username and finished time
             #binding.pry
-            puts "#{x["name"]} ............#{x["time"]}secs"
-
+            puts "______________________________________________________________"
+            puts "#{x["name"]}              #{x["time"]}secs"
+          end
+          puts "______________________________________________________________"
             puts "You have just seen the top score, do you want to play again or quit the game? Type" + " p".blue + " to play again and" + " q".red + " to quit"
             final_reply = gets.chomp
-
+            if final_reply == "p"
+            WelcomeMessage.new.introduction_message
+          else
+            puts "Thank you for playing Mastermind\n Good byebye".blue
+            system(exit)
           end
+
         elsif reply == "n"
         WelcomeMessage.new.introduction_message
       else
@@ -135,19 +147,89 @@ class TopScore
     end
   end
 # Working on having a multiplayer where they can pass a turn..... still in progress
-class Player_one
-  def guess_code_one
-    puts "What is your guess?"
-    guess_one = gets.chomp
-    return GameProcess.play_one
-  end
+class MultiPlayer
+  def game_engine_multiplayer level, name_convert_one, name_convert_two
+    start_time=Time.now # this line of code records the starting time
+    generated_code = Difficulty.code_level level  #this line of code gets the computer generated code for any the level as picked by the user
+
+      10.times do |i| # this loop is to record the number of times the user can guess at each time, the partial and exact calculation are ongoing within the loop
+
+        puts "#{name_convert_one} ".green + ", input your four guesses "
+      guess_one = (gets.chomp).split('') #this line of code gets the user's guess and convert it into an array
+      if guess_one == "ch".split('')
+        p generated_code
+      end
+        exact_match_one = 0 #counter for exact match
+        partial_match_one = 0 #counter for partial match
+        check = generated_code.zip(guess_one) #this line of code combine merges the computer generated code and the guess code and the output is saved into a check variable
+          check.each do |i| #loop through the check variable to get the elements
+          if i[0] == i[1]
+              exact_match_one += 1 #elements that has the same first index and second index are considered to be exact
+          else
+              if generated_code.include?i[1] #this line od code of check the check variable and gets the second index that is only present in the generated code, those are the partial
+                  partial_match_one += 1
+              end
+          end
+      end
+
+if exact_match_one == generated_code.length # at a point where we have the same length of computer generated code and exact match, game ends and the user won the game
+         end_time=Time.now #time stops
+         @finished_time = end_time-start_time
+         puts "CONGRATULATION, #{name_convert_one}! ".blue + " You guessed the sequence " + " #{generated_code} ".red + " in" + " #{@finished_time}".cyan + "You won the game"
+
+         TopScore.topscore_file name_convert_one, @finished_time
+
+         break # at the point where the exact match equals the computer code length, the loop breaks, if not the loops continues
+else
+puts "#{name_convert_one} ".blue + ": #{guess_one} ".cyan + " has " + "#{partial_match_one} ".magenta + " partial match with " + "#{exact_match_one} ".blue + "
+exact match in the correct positions. You have taken " + "#{i+1}".cyan + " attempt or guess(s)"
+
+    end
+
+    start_time=Time.now # this line of code records the starting time
+    #this line of code gets the computer generated code for any the level as picked by the user
+
+        puts "#{name_convert_two} ".green + ", input your four guesses "
+      guess_two = (gets.chomp).split('') #this line of code gets the user's guess and convert it into an array
+      if guess_two == "ch".split('')
+        p generated_code
+      end
+        exact_match_two = 0 #counter for exact match
+        partial_match_two = 0 #counter for partial match
+        check = generated_code.zip(guess_two) #this line of code combine merges the computer generated code and the guess code and the output is saved into a check variable
+          check.each do |i| #loop through the check variable to get the elements
+          if i[0] == i[1]
+              exact_match_two += 1 #elements that has the same first index and second index are considered to be exact
+          else
+              if generated_code.include?i[1] #this line od code of check the check variable and gets the second index that is only present in the generated code, those are the partial
+                  partial_match_two += 1
+              end
+          end
+      end
+
+if exact_match_two == generated_code.length # at a point where we have the same length of computer generated code and exact match, game ends and the user won the game
+         end_time=Time.now #time stops
+         @finished_time = end_time-start_time
+         puts "CONGRATULATION, #{name_convert_two}! ".blue + " You guessed the sequence " + " #{generated_code} ".red + " in" + " #{@finished_time}".cyan + "You won the game"
+
+         TopScore.topscore_file name_convert_two, @finished_time
+
+         break # at the point where the exact match equals the computer code length, the loop breaks, if not the loops continues
+else
+puts "#{guess_two} ".cyan + " has " + "#{partial_match_two} ".magenta + " partial match with " + "#{exact_match_two} ".blue + "
+exact match in the correct positions. You have taken " + "#{i+1}".cyan + " attempt or guess(s)"
+
+    end
+
 end
-
-
-class Player_two
-  def guess_code_one
-    puts "What is your guess?"
-    guess_two = gets.chomp
-    return GameProcess.play_two
-  end
+#Here, the loops is completed, the user has reached the number of time, so he lost the game
+puts "Both of you lost badly, " + "SORRY!".red + " Do you want to " + " Try again?".cyan + " Enter " + " (y)".blue + " for" + " Yes".blue + " or any key for " + "No\n".red
+feedback = gets.chomp
+if feedback == "y"
+  WelcomeMessage.new.introduction_message
+else
+   puts "Thank you for playing Mastermind MultiPlayer\n Good byebye".blue
+ system(exit) #this code breaks the whole of my code and leaves the entire game
+end
+end
 end
--- welcome_message.rb
+++ welcome_message.rb
@@ -3,6 +3,7 @@ require 'colorize'  # responsible for all the beautiful colors you are seeing wh
 
 class WelcomeMessage # houses both introduction and start message class
   def introduction_message # this is the introductory message method and also call the start_message method
+    puts "======================================================================"
       puts "Welcome to MASTERMIND!\n
   Would you like to " + "(p)lay".blue + ", read the" + " (i)nstructions".cyan + ", read a little" + " (b)ackground".yellow + " on
    Mastermind or " + " (q)uit?".red
@@ -13,40 +14,135 @@ class WelcomeMessage # houses both introduction and start message class
    introductory = gets.chomp
    case introductory
    when "p"
-   puts "To start the game select a level you would like to play:\n
-Enter (1) for Beginner,
-Enter (2) for Intermediate,
-Enter (3) for Advanced."
+   puts "To start the game select a level you would like to play:\n"
+   puts "===========================
+Enter (1) for" + " Beginner,".cyan + "
+Enter (2) for" + " Intermediate,".magenta + "
+Enter (3) for" + " Advanced.\n".yellow
+   puts "==========================="
     @level = gets.chomp
     if @level == "1"
-      puts "So you would like to play the game!,\n start by telling me your name\n"
+      puts "Do you want to play as a " +  "single-Player".blue + " or" + " Multi-player?".blue + " if single-Player, type" + " s".red + " and if Multi-player, type" + " m".red
+      puts "-------------------------------------------------------------------"
+      @player = gets.chomp
+
+      if @player == "s"
+
+      puts "So you would like to play the game!, start by telling me your name:\n"
+      puts "--------------------------------------------------------------------"
         @name = gets.chomp
         @name_convert = @name.upcase
+        puts "------------------------------------------------------------------"
         puts "#{@name_convert} ".green + ", I have generated a beginner sequence with four elements made up of:\n"
 puts "(y)ellow, ".yellow + " (c)yan, ".cyan + " (b)lue,".blue + " and " + " (r)ed,".red + " You are to guess the sequence in which the
 colors appeared e.g YCOR for " + "(y)ellow, ".yellow + " (c)yan,".cyan + " (b)lue,".blue + " (r)ed,".red + " You have 10 guesses to get these colors or you lose the game.
 Ready to " + " play?".blue + "\n" + " Enter your Four Color guesses:"
     GameEngine.new.play_calc_exact_partial @level, @name_convert
+
+
+  elsif @player == "m"
+
+    puts "So you would like to play the game as a multiplayer!,\n start by telling me your names\n"
+    puts "First User:".magenta
+      @name_one = gets.chomp
+      @name_convert_one = @name_one.upcase
+      puts "Second User:".magenta
+      @name_two = gets.chomp
+      @name_convert_two = @name_two.upcase
+        puts "------------------------------------------------------------------"
+      puts "#{@name_convert_one}".green + " and" + " #{@name_convert_two} ".green + ", I have generated a beginner sequence with four elements made up of:\n"
+puts "(y)ellow, ".yellow + " (c)yan, ".cyan + " (b)lue,".blue + " and " + " (r)ed,".red + " You are to guess the sequence in which the
+colors appeared e.g YCOR for " + "(y)ellow, ".yellow + " (c)yan,".cyan + " (b)lue,".blue + " (r)ed,".red + " You have 10 guesses to get these colors or you lose the game.
+Ready to " + " play?".blue + "\n" + " the first to guess correctly wins the game\n Enter your Four Color guesses:"
+  MultiPlayer.new.game_engine_multiplayer @level, @name_convert_one, @name_convert_two
+else
+  puts "You inputed the wrong key"
+  WelcomeMessage.start_message
+end
+
+
 elsif @level == "2"
-  puts "So you would like to play the game!,\n start by telling me your name\n"
+  puts "Do you want to play as a single-Player or Multi-player? if single-Player, type s and if Multi-player, type m"
+
+  @player = gets.chomp
+
+  if @player == "s"
+
+
+  puts "So you would like to play the game as a single user!,\n start by telling me your name\n"
     @name = gets.chomp
     @name_convert = @name.upcase
-  puts "#{@name_convert} ".green + ", I have generated an Intermediate sequence with four elements made up of:\n"
+      puts "------------------------------------------------------------------"
+  puts "#{@name_convert} ".green + ", I have generated an Intermediate sequence with five colors made up of:\n"
 puts "(y)ellow, ".yellow + "(c)yan, ".cyan + "(b)lue, ".blue + "(g)reen ".green + " and " + " (r)ed,".red + " You are to guess the sequence in which these colors appeared e.g YCBGR for " + " (y)ellow, ".yellow + " (c)yan, ".cyan + " (b)lue, ".blue + " (g)reen ".green + " (r)ed, ".red + " You have 10 guesses to get these colors or you lose the game. Ready to play?\n Enter your five Color guesses:"
     GameEngine.new.play_calc_exact_partial @level, @name_convert
+
+  elsif @player == "m"
+
+    puts "So you would like to play the game as a multiplayer!,\n start by telling me your names\n"
+    puts "First User:".magenta
+      @name_one = gets.chomp
+      @name_convert_one = @name_one.upcase
+      puts "Second User:".magenta
+      @name_two = gets.chomp
+      @name_convert_two = @name_two.upcase
+        puts "------------------------------------------------------------------"
+      puts "#{@name_convert_one}".green + " and" + " #{@name_convert_two} ".green + ", I have generated a beginner sequence with four elements made up of:\n"
+puts "(y)ellow, ".yellow + "(c)yan, ".cyan + "(b)lue, ".blue + "(g)reen ".green + " and " + " (r)ed,".red + " You are to guess the sequence in which the
+colors appeared e.g YCBGR for " + " (y)ellow, ".yellow + " (c)yan, ".cyan + " (b)lue, ".blue + " (g)reen ".green + " (r)ed, ".red + " You have 10 guesses to get these colors or you lose the game.
+Ready to " + " play?".blue + "\n" + " the first to guess correctly wins the game\n Enter your Four Color guesses:"
+  MultiPlayer.new.game_engine_multiplayer @level, @name_convert_one, @name_convert_two
+else
+  puts "You inputed the wrong key"
+  WelcomeMessage.start_message
+end
+
 elsif @level == "3"
-  puts "So you would like to play the game!,\n start by telling me your name\n"
+
+  puts "Do you want to play as a single-Player or Multi-player? if single-Player, type s and if Multi-player, type m"
+
+  @player = gets.chomp
+
+  if @player == "s"
+
+
+  puts "So you would like to play the game as a single user!,\n start by telling me your name\n"
     @name = gets.chomp
     @name_convert = @name.upcase
-    puts "#{@name_convert}".green + ", I have generated an Advanced sequence with four elements made up of:\n"
-puts "(y)ellow,".yellow + "(c)yan,".cyan + "(b)lue,".blue + "(g)reen ".green + "(w)hite ".white + " and " + "(r)ed,".red + " You are to guess the sequence in which these colors appeared e.g YCBGWR for " + " (y)ellow, ".yellow + " (c)yan,".cyan + " (b)lue, ".blue + " (g)reen ".green + "(w)hite ".white + "(r)ed, ".red + " You have 10 guesses to get these colors or you lose the game.
-Ready to play?\n Enter your six Color guesses:"
+      puts "------------------------------------------------------------------"
+    puts "#{@name_convert}".green + ", I have generated an Advanced sequence with six colors made up of:\n"
+puts "(y)ellow,".yellow + "(c)yan,".cyan + "(b)lue,".blue + "(g)reen ".green + "(w)hite ".white + " and " + "(r)ed,".red + " You are to guess the sequence in which these colors appeared e.g YCBGWR for " + " (y)ellow, ".yellow + " (c)yan,".cyan + " (b)lue, ".blue + " (g)reen ".green + "(w)hite ".white + "(r)ed, ".red + " You have 10 guesses to get these colors or you lose the game."
+"Ready to play?\n Enter your six Color guesses:"
       GameEngine.new.play_calc_exact_partial @level, @name_convert
+
+    elsif @player == "m"
+      puts "------------------------------------------------------------------"
+      puts "So you would like to play the game as a multiplayer!,\n start by telling me your names\n"
+      puts "First User:".magenta
+        @name_one = gets.chomp
+        @name_convert_one = @name_one.upcase
+        puts "Second User:".magenta
+        @name_two = gets.chomp
+        @name_convert_two = @name_two.upcase
+          puts "------------------------------------------------------------------"
+        puts "#{@name_convert_one}".green + " and" + " #{@name_convert_two} ".green + ", I have generated an advance sequence with six colors made up of:\n"
+  puts "(y)ellow,".yellow + "(c)yan,".cyan + "(b)lue,".blue + "(g)reen ".green + "(w)hite ".white + " and " + "(r)ed,".red + " You are to guess the sequence in which the
+  colors appeared e.g YCBGWR for " + " (y)ellow, ".yellow + " (c)yan,".cyan + " (b)lue, ".blue + " (g)reen ".green + "(w)hite ".white + "(r)ed, ".red + " You have 10 guesses to get these colors or you lose the game.
+  Ready to " + " play?".blue + "\n" + " the first to guess correctly wins the game\n Enter your Four Color guesses:"
+    MultiPlayer.new.game_engine_multiplayer @level, @name_convert_one, @name_convert_two
+  else
+    puts "You inputed the wrong key"
+    WelcomeMessage.start_message
+  end
+
 else
   puts "You have entered an invalid input"
 end
+puts "-------------------------------------------------------------------------"
    when "i" then puts "Enter a sequence of 4, 5 or 6 colors depending on the difficulty respectively containing the generated colors e.g YCBR for beginner or YCBGR for intermidiate or YCBGWR for advance.".cyan
 WelcomeMessage.new.introduction_message
+puts "-------------------------------------------------------------------------"
+puts "-------------------------------------------------------------------------"
 when "b" then puts "Just a little background on MASTERMIND Mastermind is a board game with an
 interesting history (or rather a legend?). Some game books report that it was in
 vented in 1971 by Mordecai Meirowitz, an Israeli postmaster and telecommunicatio
@@ -58,8 +154,10 @@ milar game called 'Bulls and cows' in English, and 'Numerello' in Italian... Act
 ually, the old British game 'Bulls and cows' was somewhat different from the com
 mercial version. It was played on paper, not on a board... Over 50 million copie
 s later, Mastermind is still marketed today!".yellow
+puts "-------------------------------------------------------------------------"
 WelcomeMessage.new.introduction_message
-  when "q" then puts "Thank you for playing Mastermind\n Good byebye".blue
+  when "q" then puts "Thank you for playing Mastermind\n Good byebye\n ".blue
+    puts "======================================================================"
     system('exit')
 end
   end
